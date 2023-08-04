b3daae6933b5ce7dc1887762456796c62a0f8d49
EhabMagdyy <ehabmagdy0044@gmail.com>
2023-08-05 02:03:48 +0300

    last

--- main.c
+++ main.c
@@ -7,8 +7,13 @@ int main()
 
     int selected_choice;
 
+    printf("\t*****************************************\n\
+        *             Bank DataBase             *\n\
+        *****************************************\n\n");
+
     while(1){
-        printf("\t******************\nPlease, select the operation from 1 to 7....\n");
+        printf("\t**********************************\n");
+        printf("- Please, select the operation from 1 to 7....\n");
         printf("1) Create new customer.\n");
         printf("2) Edit customer name.\n");
         printf("3) Delete a customer.\n");
@@ -16,6 +21,7 @@ int main()
         printf("5) Print a customer.\n");
         printf("6) Print all customers.\n");
         printf("7) Exit.\n");
+        printf("\t*********************************\n");
 
         printf("\n-->Enter Your choice: ");
         scanf("%d", &selected_choice);
